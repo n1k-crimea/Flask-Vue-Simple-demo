1e095c9a032be18aa40d3d476620712dcbba3cff
n1k-crimea <evloshevsky@gmail.com>
2019-12-18 17:00:37 +0300

    Simple test Flask+Vue(webpack)

---
+++ static/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <ul>
+            <li v-for="l in languages">{{ l }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'app',
+        data() {
+            return {
+                    languages: []
+            }
+        },
+        created() {
+            axios.get("/api/languages").then(r => {
+                   this.languages = r.data.languages
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
